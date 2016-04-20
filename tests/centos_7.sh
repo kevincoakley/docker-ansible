7de3be6d521b47dfddd41938fb261cdb9113a804
Kevin Coakley <kcoakley@sdsc.edu>
2016-04-20 10:54:10 -0700

    Tests for CentOS 7 and Ubuntu 15.10 containers

---
+++ tests/centos_7.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DOCKER_OUTPUT=`docker run --name my_centos7-ansible -ti kevincoakley/centos7-ansible /usr/bin/ansible --version`
+
+echo $DOCKER_OUTPUT
+
+if [[ $DOCKER_OUTPUT == *"ansible 2.0.2.0"* ]];
+then
+    exit 0;
+else
+    exit 1;
+fi
