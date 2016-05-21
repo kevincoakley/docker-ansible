cfd07b641704e19ce27fcb044bbf78242bd23764
Kevin Coakley <kcoakley@sdsc.edu>
2016-05-20 18:50:41 -0700

    Added Dockerfile for 16.04

---
+++ tests/ubuntu_1604.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DOCKER_OUTPUT=`docker run --name my_ubuntu16.04-ansible -ti kevincoakley/ubuntu16.04-ansible /usr/bin/ansible --version`
+
+echo $DOCKER_OUTPUT
+
+if [[ $DOCKER_OUTPUT == *"ansible 2.0.2.0"* ]];
+then
+    exit 0;
+else
+    exit 1;
+fi
