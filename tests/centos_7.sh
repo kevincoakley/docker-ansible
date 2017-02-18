ac54e888d6979298a0da0da08596dda20daf5456
Kevin Coakley <kcoakley@sdsc.edu>
2017-02-17 20:38:20 -0800

    Updated to Ansible 2.2.1.0

--- tests/centos_7.sh
+++ tests/centos_7.sh
@@ -4,7 +4,7 @@ DOCKER_OUTPUT=`docker run --name my_centos7-ansible -ti kevincoakley/centos7-ans
 
 echo $DOCKER_OUTPUT
 
-if [[ $DOCKER_OUTPUT == *"ansible 2.2.0.0"* ]];
+if [[ $DOCKER_OUTPUT == *"ansible 2.2.1.0"* ]];
 then
     exit 0;
 else
